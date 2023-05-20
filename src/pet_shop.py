4e0aa85690ab966b63bee5c0e38c4a170f516736
Student <student@Students-MacBook-Pro.local>
2023-05-20 13:30:23 +0100

    9 run tests

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -9,6 +9,7 @@ def find_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop["pets"]:
         if pet["name"] == pet_name:
             return pet
+       
     
 def add_or_remove_cash(pet_shop, cash):
     pet_shop["admin"]["total_cash"] += cash  
@@ -25,7 +26,30 @@ def get_stock_count(pet_shop):
     #     if len(number) == 0:
     #         count += 1
     #         return count
+
+# def get_pets_by_breed(pet_shop, breed):
+#     for pets_by_breed in breed:
+#         total_breeds += type_of_breed[breed]
+#         return (pets_by_breed)
+
+
+
+# def get_pets_by_breed(pet_shop, breed):
+#     pass
+
+# def remove_pet_by_name(pet_shop, pets):
+#     pet_shop["name"].remove("Arthur") 
+
+
+def add_pet_to_stock(pet_shop, new_pet):
+    new_pet["name"].append("haribo"),
+    new_pet["pet_type"].append("dog"),
+    new_pet["breed"].append("cockerspaniel"),
+    new_pet["price"].append("1500"),
+
+    return pet_shop["new_pet"]
             
-    
+# def add_friend(person, friend):
+#     person["friends"].append("friend")
 
     
